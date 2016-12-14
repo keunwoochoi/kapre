6230010eca51ac717fb17d7259e2abf41d71bc92
Keunwoo Choi <gnuchoi+github@gmail.com>
2016-12-14 13:39:50 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='kapre',
+      version='0.0.1',
+      description='KAPRE: Keras Audio PREprocessing',
+      url='http://github.com/keunwoo/kapre',
+      author='Keunwoo Choi',
+      author_email='gnuchoi@gmail.com',
+      licsnse='MIT',
+      packages=['dibrosa'],
+      zip_safe=False)
