c7561c575872144a008a2c81512c1c67471bd19a
Keunwoo Choi <gnuchoi+github@gmail.com>
2017-01-24 23:15:00 +0000

    add some more on init

--- kapre/__init__.py
+++ kapre/__init__.py
@@ -1,3 +1,11 @@
 # from time_frequency import Spectrogram, Melspectrogram
 # from utils import AmplitudeToDB, Normalization2D
 
+from . import time_frequency
+from . import backend
+from . import backend_keras
+from . import stft
+from . import augmentation
+from . import filterbank
+from . import utils
+
