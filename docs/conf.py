eda94a32f376fd4d52aedd4995d1a49b2b6536da
Keunwoo Choi <gnuchoi+github@gmail.com>
2017-05-31 02:36:17 +0100

    modify system path

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
-
+# sys.path.insert(0, os.path.abspath('../'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 # -- General configuration ------------------------------------------------
 
