c1ab4819245966b6c35b054fb7d00089f6879933
Keunwoo Choi <gnuchoi+github@gmail.com>
2017-05-31 01:01:26 +0100

    add mock in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,17 @@ import sys
 import os
 import shlex
 
+import sys
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
