58305516a43be1c5941f544849f5fcf4c2a8d571
Keunwoo Choi <gnuchoi+github@gmail.com>
2017-05-31 01:25:41 +0100

    even more mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,9 @@ class Mock(MagicMock):
             return MagicMock()
 
 MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas', 'tensorflow', 
-                'argparse', 'scipy', 'freetype', 'matplotlib']
+                'argparse', 'scipy', 'freetype', 'matplotlib', 'keras.acivation', 'keras.backend',
+                'keras.backend.tensorflow_backend', 'tensorflow.python.training', 
+                'tensorflow.python.training.moving_averages']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
