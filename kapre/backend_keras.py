b1dbe2c057ca980bbae7df8e04d723c309616f24
Keunwoo Choi <keunwooc@spotify.com>
2019-01-07 17:38:34 -0500

    fix max to be sample-wise

--- kapre/backend_keras.py
+++ kapre/backend_keras.py
@@ -7,13 +7,19 @@ from keras import backend as K
 def amplitude_to_decibel(x, amin=1e-10, dynamic_range=80.0):
     """[K] Convert (linear) amplitude to decibel (log10(x)).
 
-    x: Keras tensor or variable. 
+    x: Keras *batch* tensor or variable. It has to be batch because of sample-wise `K.max()`.
 
     amin: minimum amplitude. amplitude smaller than `amin` is set to this.
 
     dynamic_range: dynamic_range in decibel
+
     """
     log_spec = 10 * K.log(K.maximum(x, amin)) / np.log(10).astype(K.floatx())
-    log_spec = log_spec - K.max(log_spec)  # [-?, 0]
+    if K.ndim(x) > 1:
+        axis = tuple(range(K.ndim(x))[1:])
+    else:
+        axis = None
+
+    log_spec = log_spec - K.max(log_spec, axis=axis, keepdims=True)  # [-?, 0]
     log_spec = K.maximum(log_spec, -1 * dynamic_range)  # [-80, 0]
     return log_spec
