bcaa91bcb6c8e7e8ae56dba6cd98e8e337774c1a
server 105 <oscar.herrera@pax-ai.ae>
2020-05-13 16:27:07 +0400

    Test for Deltas added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='kapre',
-      version='0.1.7',
+      version='0.1.8',
       description='Kapre: Keras Audio Preprocessors. Keras layers for audio pre-processing in deep learning',
       author='Keunwoo Choi',
       url='http://github.com/keunwoochoi/kapre/',
