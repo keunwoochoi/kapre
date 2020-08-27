40fc47669a98f81eb0e26692bd4863e8daf287c7
Keunwoo Choi <keunwoo.choi@bytedance.com>
2020-08-27 13:28:22 -0400

    ugh

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../kapre/'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../kapre'))
 import sphinx_rtd_theme
 
 
