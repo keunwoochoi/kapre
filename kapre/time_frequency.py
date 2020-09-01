33bc7fc341f680b2e09d34d2047e7e626c4b1378
khaled <khaled.koutini@jku.at>
2020-09-01 10:46:23 +0200

    switching to args

--- kapre/time_frequency.py
+++ kapre/time_frequency.py
@@ -560,45 +560,15 @@ class FrequencyAwareConv2D(Conv2D):
         ValueError: when both `strides > 1` and `dilation_rate > 1`.
         """
     def __init__(self,
-               filters,
-               kernel_size,
-               strides=(1, 1),
-               padding='valid',
-               data_format=None,
-               dilation_rate=(1, 1),
-               groups=1,
-               activation=None,
-               use_bias=True,
-               kernel_initializer='glorot_uniform',
-               bias_initializer='zeros',
-               kernel_regularizer=None,
-               bias_regularizer=None,
-               activity_regularizer=None,
-               kernel_constraint=None,
-               bias_constraint=None,
+               *args,
                **kwargs):
-        if groups > 1:
+        if ('groups' in kwargs and kwargs.get('groups')>1 ) or \
+             (len(args) >= 7 and args[7]>1):
             warnings.warn(
                 'Grouped Conv2D are not supported.'
                 'Only the first group will be frequency aware.'
             )
-        super(FrequencyAwareConv2D, self).__init__(
-            filters=filters,
-            kernel_size=kernel_size,
-            strides=strides,
-            padding=padding,
-            data_format=data_format,
-            dilation_rate=dilation_rate,
-            groups=groups,
-            activation=activation,
-            use_bias=use_bias,
-            kernel_initializer=kernel_initializer,
-            bias_initializer=bias_initializer,
-            kernel_regularizer=kernel_regularizer,
-            bias_regularizer=bias_regularizer,
-            activity_regularizer=activity_regularizer,
-            kernel_constraint=kernel_constraint,
-            bias_constraint=bias_constraint,
+        super(FrequencyAwareConv2D, self).__init__(*args,
             **kwargs)
 
 
