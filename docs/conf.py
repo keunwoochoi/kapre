c008bf0b0e6dbcd51c3189123c221f473dde1cfa
Keunwoo Choi <keunwoo.choi@bytedance.com>
2021-03-18 17:51:36 -0400

    re-travis

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 import sphinx_rtd_theme
+
 autodoc_mock_imports = ['tensorflow', 'librosa', 'numpy']
 autodoc_member_order = 'bysource'
 
